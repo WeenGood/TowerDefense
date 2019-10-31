3c8cea523074f84a57b269b164ffa90eb75e579c
Maxim Sechnoy <inquisitor914@gmail.com>
2019-10-31 20:00:20 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,54 @@
 let canvas = document.getElementById("Game");
 let ctx = canvas.getContext("2d");
 
+start = true;
+life = 20;
+gold = 0;
+
 //Вызов по клику
 document.addEventListener("click", ClickHandler);
 function ClickHandler(click) {
-
+    //
 }
+
+//База
+class Base {
+
+    constructor(x, y) {
+        this.pos = [x, y];
+        this.size = 100;
+    }
+
+    getDamage() {
+        life = life - 1;
+        if (life < 0) {
+            start = false;
+        }
+    }
+
+    mineGold() {
+        gold = gold + 10;
+    }
+}
+
+//Башня
+class Tower {
+
+    constructor(x, y) {
+        this.pos = [x, y];
+        this.size = 25;
+        this.radius = 100;
+        this.damage = 5;
+    }
+    //
+}
+
 //Отрисовка
-function draw() {
-
+function Draw() {
+    while (start) {
+        //
+    }
 }
+
 //Обновление с частотой 60 кадров в секунду
-setInterval(draw, 16.7);
+setInterval(Draw, 16.7);
