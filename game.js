01291a1afb950dca2e651a1e7e5db0bf4cb4e3f6
Maxim Sechnoy <inquisitor914@gmail.com>
2019-10-31 19:23:00 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
 let canvas = document.getElementById("Game");
 let ctx = canvas.getContext("2d");
 
-
+//Вызов по клику
 document.addEventListener("click", ClickHandler);
-
-function ClickHandler(k) {
+function ClickHandler(click) {
 
 }
+//Отрисовка
 function draw() {
 
 }
+//Обновление с частотой 60 кадров в секунду
 setInterval(draw, 16.7);
